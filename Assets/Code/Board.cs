abf8f5ff87e700d36d9e29f85a0820fb0a8e98d3
Dustin Kaban <dustinkaban@gmail.com>
2019-03-01 10:26:03 -0800

    First Upload of Conways Game Of Life Unity

---
+++ Assets/Code/Board.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Developer : Dustin Kaban
+/// Date : February 27th, 2019
+/// Purpose : Re-Create the Game Of Life in Unity using C#
+/// </summary>
+
+public class Board : MonoBehaviour
+{
+    [NonSerialized]
+    public Cell[] CellArray;
+    [NonSerialized]
+    public bool BoardCompleted = false;
+    [NonSerialized]
+    public bool Running = true;
+    [NonSerialized]
+    public float TurnDuration = 0.1f;
+
+    public GameObject CellPrefabObject;
+
+    private readonly int _width = 50;
+    private readonly int _height = 50;
+    private readonly float _percentAlive = 0.10f;
+
+    public static Board Instance { get; protected set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        CellArray = new Cell[_width * _height];
+    }
+
+    private void Start()
+    {
+        CreateGrid();
+        RandomizeAliveOrDead();
+        BoardCompleted = true;
+    }
+
+    private void CreateGrid()
+    {
+        int cellIndex = 0;
+        for(int w=0;w<_width;w++)
+        {
+            for(int h=0;h<_height;h++)
+            {
+                CreateCell(w, h, cellIndex);
+                cellIndex++;
+            }
+        }
+    }
+
+    private void CreateCell(float w, float h, int index)
+    {
+        Vector2 cellPosition = new Vector2(w, h);
+        GameObject cellGameObject = GameObject.Instantiate(CellPrefabObject) as GameObject;
+        cellGameObject.transform.SetParent(this.transform);
+        Cell cell = cellGameObject.GetComponent<Cell>();
+        cell.InitCell(cellPosition, false,(w+" "+h),index,w,h);
+        CellArray[index] = cell;
+    }
+
+    private void RandomizeAliveOrDead()
+    {
+        for(int i=0;i<CellArray.Length;i++)
+        {
+            if(UnityEngine.Random.value <= _percentAlive)
+            {
+                CellArray[i].SetAlive();
+            }
+            else
+            {
+                CellArray[i].SetDead();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.S))
+        {
+            Running = false;
+        }
+
+        if(Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
---
+++ Assets/Code/Cell.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Developer : Dustin Kaban
+/// Date : February 27th, 2019
+/// Purpose : Re-Create the Game Of Life in Unity using C#
+/// </summary>
+
+public class Cell : MonoBehaviour
+{
+    [NonSerialized]
+    public Vector2 Position;
+    [NonSerialized]
+    public bool Alive = false;
+    [NonSerialized]
+    public string CellLocation = "";
+    [NonSerialized]
+    public int CellIndex = 0;
+    [NonSerialized]
+    public float X = 0;
+    [NonSerialized]
+    public float Y = 0;
+
+    public SpriteRenderer Renderer;
+
+    public void InitCell(Vector2 position, bool alive, string cellLocation, int cellIndex,float x, float y)
+    {
+        Position = position;
+        Alive = alive;
+        CellLocation = cellLocation;
+        this.gameObject.transform.position = Position;
+        this.gameObject.name = cellLocation;
+        CellIndex = cellIndex;
+        X = x;
+        Y = y;
+    }
+
+    public void SetAlive()
+    {
+        Alive = true;
+        Renderer.color = Color.white;
+    }
+
+    public void SetDead()
+    {
+        Alive = false;
+        Renderer.color = Color.black;
+    }
+
+    private void Start()
+    {
+        StartCoroutine("CheckNeighbours");
+    }
+
+    private IEnumerator CheckNeighbours()
+    {
+        while (Board.Instance.Running)
+        {
+            yield return new WaitForSeconds(Board.Instance.TurnDuration);
+
+            if (Board.Instance.BoardCompleted)
+            {
+                int neighbourAliveCount = 0;
+                int neighbourCount = 0;
+                for(int i=0;i<Board.Instance.CellArray.Length;i++)
+                {
+                    if (i != CellIndex)
+                    {
+                        if ((Board.Instance.CellArray[i].X == X + 1 && Board.Instance.CellArray[i].Y == Y)
+                            || (Board.Instance.CellArray[i].X == X - 1 && Board.Instance.CellArray[i].Y == Y)
+                            || (Board.Instance.CellArray[i].Y == Y + 1 && Board.Instance.CellArray[i].X == X)
+                            || (Board.Instance.CellArray[i].Y == Y - 1 && Board.Instance.CellArray[i].X == X)
+                            || (Board.Instance.CellArray[i].Y == Y + 1 && Board.Instance.CellArray[i].X == X + 1)
+                            || (Board.Instance.CellArray[i].Y == Y + 1 && Board.Instance.CellArray[i].X == X - 1)
+                            || (Board.Instance.CellArray[i].Y == Y - 1 && Board.Instance.CellArray[i].X == X + 1)
+                            || (Board.Instance.CellArray[i].Y == Y - 1 && Board.Instance.CellArray[i].X == X - 1))
+                        {
+                            if (Board.Instance.CellArray[i].Alive)
+                            {
+                                neighbourAliveCount++;
+                            }
+
+                            neighbourCount++;
+
+                            if (neighbourCount >= 8) i = Board.Instance.CellArray.Length;
+                        }
+                    }
+                }
+                ApplyRules(neighbourAliveCount);
+            }
+        }
+    }
+
+    private void ApplyRules(int aliveCount)
+    {
+        if(Alive)
+        {
+            if (aliveCount <= 1)
+            {
+                SetDead();
+            }
+            else if(aliveCount == 2 || aliveCount == 3)
+            {
+                SetAlive();
+            }
+            else if(aliveCount >= 3)
+            {
+                SetDead();
+            }
+        }
+        else
+        {
+            if(aliveCount == 3)
+            {
+                SetAlive();
+            }
+        }
+    }
+
+    //Loop through the cell list and find neighbours
+    //A Neighbour is
+    //__________________________
+    //1. is only 1 X away + or -
+    //2. is only 1 Y away + or -
+    //3. is only 1 Y away + or - && is only 1 X away + or -
+    //4. is only 1 X away + or - && is only 1 Y away + or -
+    //__________________________
+
+    // Wikipedia Rules
+    // Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+    // Any live cell with two or three live neighbors lives on to the next generation.
+    // Any live cell with more than three live neighbors dies, as if by overpopulation.
+    // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+    //** NEED TO IMPLEMENT THIS **
+    // The initial pattern constitutes the seed of the system. The first generation is created by applying the above 
+    // rules simultaneously to every cell in the seed; births and deaths occur simultaneously, and the discrete moment at 
+    // which this happens is sometimes called a tick. Each generation is a pure function of the preceding one. The rules 
+    // continue to be applied repeatedly to create further generations.
+}
